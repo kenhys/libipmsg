1f4e6221df28067f7882b59a22d1430507c2c24f
Kuninobu Niki <nikikuni@yahoo.co.jp>
2006-12-09 14:13:25 +0000

    Move GetCommandString method from IpMessengerAgentImpl. Add IpMsgDumpPacket method.

--- src/PrivateUtils.cpp
+++ src/PrivateUtils.cpp
@@ -1,6 +1,8 @@
 #if defined(DEBUG) || defined(INFO)
 #include <stdio.h>
 #include <ctype.h>
+#include <IpMessenger.h>
+#include <ipmsg.h>
 
 /**
  * バッファをプリントする。
@@ -10,7 +12,7 @@
  * @param size バッファサイズ
  * 注：このメソッドはスレッドセーフでない。
  */
-void
+inline void
 IpMsgPrintBuf( char* bufname, char *buf, int size )
 {
 	int continue_count = 0;
@@ -43,5 +45,56 @@ IpMsgPrintBuf( char* bufname, char *buf, int size )
 	}
 	printf( "]\n" );
 	fflush(stdout);
+}
+
+/**
+ * コマンド文字列を返す。
+ * @param cmd コマンド
+ * @retval コマンド文字列
+ */
+string
+GetCommandString(long cmd )
+{
+	switch( cmd ){
+		case IPMSG_NOOPERATION:     return "IPMSG_NOOPERATION";
+		case IPMSG_BR_ENTRY:        return "IPMSG_BR_ENTRY";
+		case IPMSG_BR_EXIT:         return "IPMSG_BR_EXIT";
+		case IPMSG_ANSENTRY:        return "IPMSG_ANSENTRY";
+		case IPMSG_BR_ABSENCE:      return "IPMSG_BR_ABSENCE";
+		case IPMSG_BR_ISGETLIST:    return "IPMSG_BR_ISGETLIST";
+		case IPMSG_OKGETLIST:       return "IPMSG_OKGETLIST";
+		case IPMSG_GETLIST:         return "IPMSG_GETLIST";
+		case IPMSG_ANSLIST:         return "IPMSG_ANSLIST";
+		case IPMSG_BR_ISGETLIST2:   return "IPMSG_BR_ISGETLIST2";
+		case IPMSG_SENDMSG:         return "IPMSG_SENDMSG";
+		case IPMSG_RECVMSG:         return "IPMSG_RECVMSG";
+		case IPMSG_READMSG:         return "IPMSG_READMSG";
+		case IPMSG_DELMSG:          return "IPMSG_DELMSG";
+		case IPMSG_ANSREADMSG:      return "IPMSG_ANSREADMSG";
+		case IPMSG_GETINFO:         return "IPMSG_GETINFO";
+		case IPMSG_SENDINFO:        return "IPMSG_SENDINFO";
+		case IPMSG_GETABSENCEINFO:  return "IPMSG_GETABSENCEINFO";
+		case IPMSG_SENDABSENCEINFO: return "IPMSG_SENDABSENCEINFO";
+		case IPMSG_GETFILEDATA:     return "IPMSG_GETFILEDATA";
+		case IPMSG_RELEASEFILES:    return "IPMSG_RELEASEFILES";
+		case IPMSG_GETDIRFILES:     return "IPMSG_GETDIRFILES";
+		case IPMSG_GETPUBKEY:       return "IPMSG_GETPUBKEY";
+		case IPMSG_ANSPUBKEY:       return "IPMSG_ANSPUBKEY";
+	}
+	return "no match";
+}
+
+void
+IpMsgDumpPacket( Packet packet, struct sockaddr_in sender_addr ){
+	printf( ">> R E C V >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+	printf( "send from %s(%d)\n", inet_ntoa( sender_addr.sin_addr ), ntohs( sender_addr.sin_port ) );
+	printf( "VersionNo    [%ld]\n", packet.VersionNo() );
+	printf( "PacketNo     [%ld]\n", packet.PacketNo() );
+	printf( "CommandMode  [%ld][%s]\n", packet.CommandMode(), GetCommandString( packet.CommandMode() ).c_str() );
+	printf( "CommandOption[%ld]\n", packet.CommandOption() );
+	printf( "HostName     [%s]\n", packet.HostName().c_str() );
+	printf( "UserName     [%s]\n", packet.UserName().c_str() );
+	IpMsgPrintBuf("Option", (char *)packet.Option().c_str(), packet.Option().length() );
+	printf( "<< R E C V <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n\n");
 }
 #endif
