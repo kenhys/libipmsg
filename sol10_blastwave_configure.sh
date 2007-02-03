e5af41edeb9af143e61a720f6d732335232a911e
Kuninobu Niki <nikikuni@yahoo.co.jp>
2007-02-03 19:02:39 +0000

    Update:Port for Solaris 10(blastwave).

---
+++ sol10_blastwave_configure.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+AR="/opt/csw/bin/gar" CC="/opt/csw/gcc3/bin/gcc" CXX="/opt/csw/gcc3/bin/g++" CFLAGS="-I/opt/csw/include " CXXFLAGS="-I/opt/csw/include" LDFLAGS="-L/opt/csw/lib" ./configure
