8bba9608d3a7dbbaded3a7def6f83ef59e8d9e83
Kuninobu Niki <nikikuni@yahoo.co.jp>
2007-02-03 17:12:58 +0000

    Obsolate...

--- sol10_configure.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-CC="/opt/SUNWspro/bin/cc" CXX="/opt/SUNWspro/bin/CC" CFLAGS="-I/usr/sfw/include -I/opt/fsw4sun/include -D_POSIX_PTHREAD_SEMANTICS" CXXFLAGS="-I/usr/sfw/include -I/opt/fsw4sun/include -D_POSIX_PTHREAD_SEMANTICS" LDFLAGS="-L/opt/fsw4sun/lib -L/opt/fsw4snu/ssl/lib -L/usr/sfw/lib" ./configure --prefix=/opt/fsw4sun
